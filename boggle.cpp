a9a9ed5a63cf1e93f30ddf5f5ae184e4517771ff
annikataylor <annitaylor01@gmail.com>
2023-03-14 13:35:43 -0600

    1

--- boggle.cpp
+++ boggle.cpp
@@ -10,62 +10,204 @@ Boggle::~Boggle()
   // TODO
 }
 
+// returns stringstream that can be sent to cout in main.cpp
 string Boggle::getBoardString()
 {
-  // TODO
+  int x, y = 0;
+  stringstream boardStr;
+  for(y = 0; y < SIZE; y++) {
+      for(x = 0; x < SIZE; ) {
+          boardStr<< board[x][y];
+          if(x != SIZE-1)
+              boardStr << " ";
+      }
+      if(y != SIZE-1)
+          boardStr << endl;
+  }
+  return boardStr.str();
 }
 
+// uses the current time to seed the random number generator & then fills the array with random letters from alpha
 void Boggle::createRandomBoard()
 {
-  // TODO
+    srand(time(NULL));
+    string alpha = "abcdefghijklmnopqrstuvwxyz";
+    int x, y = 0;
+    for(x = 0; x < SIZE; x++){
+        for(y = 0; y < SIZE; y++) {
+            board[x][y] = alpha[rand() % alpha.size()];
+        }
+    }
 }
 
 bool Boggle::importBoard(string file_name)
 {
-  // TODO
+    ifstream ifs;
+    string line;
+    int x, y = 0;
+    string boardValues[SIZE][SIZE];
+
+    //open file
+    ifs.open(file_name, ifstream::in);
+    if(!ifs.is_open()){
+        cout << "file " << file_name << " failed to open" << endl;
+        return false;
+    }
+
+    for(y = 0; y < SIZE; y++) {
+        getline(ifs, line);
+        if(ifs.bad() || ifs.fail()) {
+            cout << "line read fail" << endl;
+            return false;
+        }
+        while(line[line.size()-1] == ' ' || line[line.size()-1] == '\n' || line[line.size()-1] == '/r')
+            line = line.substr(0, line.size()-1);
+        if(line.size() != SIZE*2-1) {
+            cout << "line wrong size: actual " << ", expected: " << SIZE*2-1 << endl;
+            return false;
+        }
+        for(x = 0; x < SIZE; x++) {
+            boardValues[x][y] = line[x*2];
+        }
+    }
+    // check for no extra lines
+    getline(ifs, line);
+    if(!ifs.bad() && !ifs.fail()) {
+        cout << "too many extra lines found" << endl;
+        return false;
+    }
+
+    setBoard(boardValues);
+    return true;
 }
 
 bool Boggle::importDictionary(string file_name)
 {
-  // TODO
+    ifstream ifs;
+    string line;
+
+    //open file
+    ifs.open(file_name, ifstream::in);
+    if(!ifs.is_open())
+        return false;
+
+    while(getline(ifs, line)) {
+        while (line[line.size() - 1] == ' ' || line[line.size() - 1] == '\n' || line[line.size() - 1] = '\r')
+            line = line.substr(0, line.size() - 1);
+        transform(line.begin(), line.end(), line.begin(), ::tolower);
+        dictionary.push_back(line);
+    }
+    return true;
 }
+
 
 void Boggle::setMinWordLength(int length)
 {
-  // TODO
+    minWordLength = length;
 }
 
 bool Boggle::isWord(string word)
 {
-  // TODO
+    return findPrefix(word, 0, dictionary.size(), true);
 }
 
 bool Boggle::isPrefix(string prefix)
 {
-  // TODO
+    return findPrefix(prefix, 0, dictionary.size(), false);
 }
 
 bool Boggle::findPrefix(string root, int start, int end, bool fullWordOnly)
 {
-  // TODO
+  // return false if indexes become invalid
+  if(start > end)
+      return false;
+
+  int mid = (start > end)/2;
+  string middle = dictionary[mid];
+
+  //return true if is valid word and is at least minWordLength
+  if(middle == root && root.size() >= minWordLength)
+      return true;
+
+  // recursive call
+  if(root > middle)
+      return findPrefix(root, mid+1, end, fullWordOnly);
+  else
+      return findPrefix(root, start, mid+1, fullWordOnly);
 }
 
 set<string> Boggle::solveBoard()
 {
-  // TODO
+    set<string> words;
+    int x, y = 0;
+
+    //start search at each possible starting position
+    for(x = 0; x < SIZE; x++) {
+        for(y = 0; y < SIZE; y++) {
+            unordered_set<string> path;
+            findWords("", path, words,x, y);
+        }
+    }
+    return words;
 }
 
-void Boggle::findWords(string root, unordered_set<string> path, set<string>& wordsFound, int x, int y)
-{
-  // TODO
+void Boggle::findWords(string root, unordered_set<string> path, set<string>& wordsFound, int x, int y) {
+    // if position not on board, return
+    if (x < 0 || x >= SIZE)
+        return;
+    if (y < 0 || y >= SIZE)
+        return;
+
+    // if position has already been visited for this root, return
+    stringstream ss;
+    ss << "(" << x << ", " << y << ")";
+    string pos = ss.str();
+    if (path.find(pos) != path.end())
+        return;
+
+    // Add position to path and add the character to root string
+    path.insert(pos);
+    root = root.append(board[x][y]);
+
+    // If root is a word, add it to solution set
+    if (isWord(root))
+        wordsFound.insert(root);
+
+    // If it is a prefix, recursively check all paths
+    if (isPrefix(root)) {
+        findWords(root, path, wordsFound, x - 1, y); // Left
+        findWords(root, path, wordsFound, x + 1, y); // Right
+        findWords(root, path, wordsFound, x, y + 1); // Down
+        findWords(root, path, wordsFound, x, y - 1); // Up
+        findWords(root, path, wordsFound, x - 1, y - 1); // NW
+        findWords(root, path, wordsFound, x + 1, y - 1); // NE
+        findWords(root, path, wordsFound, x - 1, y + 1); // SW
+        findWords(root, path, wordsFound, x + 1, y + 1); // SE
+    }
 }
 
 void Boggle::clear()
 {
-  // TODO
+    dictionary.clear();
+    minWordLength = 3;
+    int x, y = 0;
+    for(x = 0; x < SIZE; x++)
+    {
+        for(y = 0; y < SIZE; y++)
+        {
+            board[x][y] = "a";
+        }
+    }
 }
 
 void Boggle::setBoard(string boardValues[SIZE][SIZE])
 {
-  // TODO
+    int x, y = 0;
+    for(x = 0; x < SIZE; x++)
+    {
+        for(y = 0; y < SIZE; y++)
+        {
+            board[x][y] = boardValues[x][y];
+        }
+    }
 }
